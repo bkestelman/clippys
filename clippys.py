0bbfd2167542c785766247694d3510535549e135
bkestelman <bkestelman@outlook.com>
2018-09-26 19:36:03 -0400

    change hotkeys, clean up code

--- clippys.py
+++ clippys.py
@@ -4,24 +4,33 @@ import pyperclip as clipboard
 clip_keys = ['0', '9', '8']
 clips = {}
 
+copy_hotkeys = {k: f'ctrl+<+{k}' for k in clip_keys}
+paste_hotkeys = {k: f'ctrl+>+{k}' for k in clip_keys}
+
 def copy(k):
-	print(f'detected ctrl+c+{k}')
+	print(f'detected {copy_hotkeys[k]}')
 	keyboard.send('ctrl+c') 	# copy selected text
 	clips[k] = clipboard.paste()	# into the corresponding clipboard
 	print('clips:')
 	print(clips)
 
 def paste(k):
-	print(f'detected ctrl+b+{k}')
+	print(f'detected {paste_hotkeys[k]}')
 	clipboard.copy(clips[k]) 	# take text from corresponding clipboard
 	keyboard.send('ctrl+v') 	# paste it
 
 print('setting up hotkeys')
 for k in clip_keys:
 	print(f'k={k}')
-	keyboard.add_hotkey(f'ctrl+c+{k}', copy, args=[k])
+	keyboard.add_hotkey(copy_hotkeys[k], copy, args=[k])
 
 for k in clip_keys:
-	keyboard.add_hotkey(f'ctrl+b+{k}', paste, args=[k])
+	keyboard.add_hotkey(paste_hotkeys[k], paste, args=[k])
+
+print('copy_hotkeys')
+print(copy_hotkeys)
+print('paste_hotkeys')
+print(paste_hotkeys)
 
 keyboard.wait()
+
