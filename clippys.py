caf42c5330fba584758bf77047c4693428405a08
bkestelman <bkestelman@outlook.com>
2018-09-30 23:02:31 -0400

    managing clipboard with win32 api

--- clippys.py
+++ clippys.py
@@ -1,31 +1,42 @@
+'''
+Use:
+User must first copy text normally (ctrl+c) to put it in standard clipboard.
+Then use one of the copy_hotkeys to copy from the standard clipboard to one of the managed clipboards.
+Then use one of the paste_hotkeys to paste from a managed clipboard to the standard clipboard.
+Then paste normally (ctrl+v)
+'''
 import keyboard
-import pyperclip as clipboard
+import win32.win32clipboard as clipboard
 
 clip_keys = ['0', '9', '8']
 clips = {}
 
-copy_hotkeys = {k: f'ctrl+<+{k}' for k in clip_keys}
-paste_hotkeys = {k: f'ctrl+>+{k}' for k in clip_keys}
+copy_hotkeys = {k: f'ctrl+alt+c+{k}' for k in clip_keys}
+paste_hotkeys = {k: f'ctrl+alt+v+{k}' for k in clip_keys}
 
-def copy(k):
+def copyTo(k):
 	print(f'detected {copy_hotkeys[k]}')
-	keyboard.send('ctrl+c') 	# copy selected text
-	clips[k] = clipboard.paste()	# into the corresponding clipboard
+	clipboard.OpenClipboard()
+	clips[k] = clipboard.GetClipboardData()
+	clipboard.CloseClipboard()
 	print('clips:')
 	print(clips)
 
-def paste(k):
+def pasteFrom(k):
 	print(f'detected {paste_hotkeys[k]}')
-	clipboard.copy(clips[k]) 	# take text from corresponding clipboard
-	keyboard.send('ctrl+v') 	# paste it
+	clipboard.OpenClipboard()
+	clipboard.EmptyClipboard() 
+	print(f'putting {clips[k]} in clipboard')
+	clipboard.SetClipboardText(clips[k])
+	clipboard.CloseClipboard()
 
 print('setting up hotkeys')
 for k in clip_keys:
 	print(f'k={k}')
-	keyboard.add_hotkey(copy_hotkeys[k], copy, args=[k])
+	keyboard.add_hotkey(copy_hotkeys[k], copyTo, args=[k])
 
 for k in clip_keys:
-	keyboard.add_hotkey(paste_hotkeys[k], paste, args=[k])
+	keyboard.add_hotkey(paste_hotkeys[k], pasteFrom, args=[k])
 
 print('copy_hotkeys')
 print(copy_hotkeys)
