23e26df051da9d4a7310c66d0fbd464bf4200b06
bkestelman <bkestelman@outlook.com>
2018-10-01 12:39:58 -0400

    works, with manual copy/paste

--- clippys.py
+++ clippys.py
@@ -4,28 +4,26 @@ import pyperclip as clipboard
 clip_keys = ['0', '9', '8']
 clips = {}
 
-copy_hotkeys = {k: f'ctrl+<+{k}' for k in clip_keys}
-paste_hotkeys = {k: f'ctrl+>+{k}' for k in clip_keys}
+copy_hotkeys = {k: f'ctrl+alt+c+{k}' for k in clip_keys}
+paste_hotkeys = {k: f'ctrl+alt+v+{k}' for k in clip_keys}
 
-def copy(k):
+def copyTo(k):
 	print(f'detected {copy_hotkeys[k]}')
-	keyboard.send('ctrl+c') 	# copy selected text
 	clips[k] = clipboard.paste()	# into the corresponding clipboard
 	print('clips:')
 	print(clips)
 
-def paste(k):
+def pasteFrom(k):
 	print(f'detected {paste_hotkeys[k]}')
 	clipboard.copy(clips[k]) 	# take text from corresponding clipboard
-	keyboard.send('ctrl+v') 	# paste it
 
 print('setting up hotkeys')
 for k in clip_keys:
 	print(f'k={k}')
-	keyboard.add_hotkey(copy_hotkeys[k], copy, args=[k])
+	keyboard.add_hotkey(copy_hotkeys[k], copyTo, args=[k])
 
 for k in clip_keys:
-	keyboard.add_hotkey(paste_hotkeys[k], paste, args=[k])
+	keyboard.add_hotkey(paste_hotkeys[k], pasteFrom, args=[k])
 
 print('copy_hotkeys')
 print(copy_hotkeys)
